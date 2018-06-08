59aedc85a74e31a564daa18414b7968bcf7da980
scresh <screshowski@gmail.com>
2018-06-09 00:50:33 +0200

    Add real parallel computing :)

--- main.py
+++ main.py
@@ -1,41 +1,77 @@
+#!/usr/bin/env python
+# coding=utf-8
+import math
+
 import xxhash
-import threading
+from mpi4py import MPI
 import random
-import time
+import argparse
+
+CHAR_BITS = 16
+GLOBAL_TAG = 1337
 
 
-def random_plaintext(n):
-    max_range = 16 ** n - 1
-    return ('0' * 3 + hex(random.randint(0, max_range))[2:])[-4:]
+def generate_chain(chains, steps, n):
+    results = ''
+    plaintext = ''
 
-
-def generate_chain(rainbow_file, file_lock, steps, chains):
-    for _ in xrange(chains):
-        plaintext = random_plaintext(4)
+    for c in chains:
         for _ in xrange(steps - 1):
-            plaintext = xxhash.xxh64(plaintext).hexdigest()[:4]
+            plaintext = xxhash.xxh64(c).hexdigest()[:n]
         hex_hash = xxhash.xxh64(plaintext).hexdigest()
-
-        file_lock.acquire()
-        rainbow_file.write(plaintext + ":" + hex_hash + "\n")
-        file_lock.release()
+        results += plaintext + ":" + hex_hash + "\n"
+    return results
 
 
 def main():
-    n_threads = 16
-    file_lock = threading.Lock()
-    steps = 256
-    chains = 32
-    rainbow_file = open("rt.txt", "a")
-    start = time.time()
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    piece = None
 
-    threads = []
-    for thread_id in xrange(n_threads):
-        threads.append(threading.Thread(target=generate_chain, args=(rainbow_file, file_lock, steps, chains)))
-        threads[thread_id].start()
+    parser = argparse.ArgumentParser(description='A simple rainbow table generator supporting xxHash')
+    parser.add_argument('-steps', action="store", help='Number of steps in chain', dest='steps', type=int, default=None)
+    parser.add_argument('-length', action="store", help='Password length', dest='length', type=int, default=4)
 
-    stop = time.time()
-    print stop - start
+    length = parser.parse_args().length
+    steps = [parser.parse_args().steps, 2 ** (2 * length)][parser.parse_args().steps is None]
+
+    if rank == 0:
+        parser.add_argument('-chains', action="store", help='Number of chains', dest='chains', type=int, default=None)
+        chains = [parser.parse_args().chains, (16 ** length) / (steps * size)][parser.parse_args().chains is None]
+
+        ceil_no = chains % size
+        floor_no = size - ceil_no
+        ceil = int(math.ceil(float(chains) / float(size)))
+        floor = int(math.floor(float(chains) / float(size)))
+
+        plain_array = list(set(map(lambda _: ('0' * 3 + hex(random.randint(0, CHAR_BITS ** length - 1))[2:])[-length:],
+                                   xrange(2 * chains))))[:chains]
+
+        index = 0
+        for i in xrange(size):
+            add = [floor, ceil][i < ceil_no]
+            data = plain_array[index:index + add]
+            if index != 0:
+                comm.send(data, dest=i, tag=GLOBAL_TAG)
+            else:
+                piece = data
+            index += add
+    else:
+        piece = comm.recv(source=0, tag=GLOBAL_TAG)
+
+    if rank == 0:
+        result = generate_chain(piece, steps, length)
+
+        for i in xrange(1, size):
+            result += comm.recv(source=i, tag=GLOBAL_TAG)
+
+        parser.add_argument('-file', action="store", help='Output filename', dest='file', default='rt.txt')
+        rainbow_file = open(parser.parse_args().file, "w")
+        rainbow_file.write(result)
+
+    else:
+        comm.send(generate_chain(piece, steps, length), dest=0, tag=GLOBAL_TAG)
 
 
 if __name__ == "__main__":
